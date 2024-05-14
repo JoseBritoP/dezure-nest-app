c49c0ebd3cdaad7bc6ff3eb848b0bfbcd08250f5
José Brito <josealbritose@gmail.com>
2024-05-14 20:45:45 -0300

    Get Products - Pagination

---
+++ src/products/products.controller.ts
@@ -0,0 +1,34 @@
+import { Controller, Get, Query } from '@nestjs/common';
+import { ProductsService } from './products.service';
+import { ApiTags } from '@nestjs/swagger';
+
+@ApiTags('Products')
+@Controller('api/products')
+export class ProductsController {
+  constructor(private readonly productsService: ProductsService) {}
+
+  @Get()
+  findAll(
+    // @Query('creatorId') creatorId?: number,
+    // @Query('name') name?: string,
+    // @Query('minPrice') minPrice?: number,
+    // @Query('maxPrice') maxPrice?: number,
+    // @Query('inStock') inStock?: boolean,
+    @Query('limit')   limit?:string,
+    @Query('page')    page?:string
+  ) {
+
+    // const filters = {
+    //   creatorId,
+    //   name,
+    //   minPrice,
+    //   maxPrice,
+    //   inStock
+    // }
+    // console.log(filters)
+    // return this.productsService.findAll(filters);
+
+
+    return this.productsService.findAll(limit,page);
+  }
+}
---
+++ src/products/products.service.ts
@@ -0,0 +1,39 @@
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Product } from 'src/manage-products/entities/product.entity';
+import { Repository } from 'typeorm';
+// import { ProductsFilters } from './products-filters.dto';
+
+@Injectable()
+export class ProductsService {
+  constructor(@InjectRepository(Product) 
+    private productRepository:Repository<Product>
+  ){}
+
+  
+  async findAll(limit:string,page:string){
+
+
+    const limitValue = Number(limit) <= 0 || limit === undefined || Number.isNaN(+limit) ? 5 : Number(limit);
+    const pageValue =  Number(page)  <= 0 || page  === undefined || Number.isNaN(+page) ? 1 : Number(page);
+    
+    const skip = (pageValue-1) * limitValue;
+
+    if(Number.isNaN(skip)) return new HttpException('Invalid values',HttpStatus.BAD_REQUEST);
+    
+
+    const products = await this.productRepository.find({
+      skip,
+      take:limitValue    
+    });
+
+    const productsCount = await this.productRepository.count();
+
+    return {
+      limit:limitValue,
+      currentPage:pageValue,
+      totalPages:productsCount/limitValue,
+      data:products
+    }
+  }
+}
