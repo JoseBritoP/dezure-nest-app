7b9d37a0f26f430276b0be5907f2a7f0dcba382a
José Brito <josealbritose@gmail.com>
2024-05-14 08:28:26 -0300

    TypeORM - Conection DB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
+import { UsersModule } from './users/users.module';
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST || 'localhost',
+      port: process.env.DB_PORT || 5432,
+      password: process.env.DB_PASSWORD || '1234',
+      username: process.env.DB_USERNAME || 'postgres',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      database: process.env.DB_NAME || 'nest-int',
+      synchronize: true,
+      logging: false,
+      cache:true
+    }),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
