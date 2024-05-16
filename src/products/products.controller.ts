477e42e8b9ebc09a74c8d1d6820db3395292e083
José Brito <josealbritose@gmail.com>
2024-05-16 16:01:40 -0300

    Swagger

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,12 +1,23 @@
 import { Controller, Get, Query } from '@nestjs/common';
 import { ProductsService } from './products.service';
-import { ApiQuery, ApiTags } from '@nestjs/swagger';
+import { ApiQuery, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { QueryValuesDto } from './dto/products-filters.dto';
+import { Product } from 'src/manage-products/entities/product.entity';
 
 @ApiTags('Products')
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
+  @ApiResponse({
+    status:200,
+    type:class Response {
+      filters:QueryValuesDto
+      limit:number
+      currentPage:number
+      totalItems:number
+      data:Product[]
+    }
+  })
   @Get()
   @ApiQuery({ name: 'limit', required: false, description: 'Maximum number of results to return',type:Number })
   @ApiQuery({ name: 'page', required: false, description: 'Page number for pagination' ,type:Number})
